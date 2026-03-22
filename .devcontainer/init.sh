2dd590c3c4195a0a8b6763a33ab5f0833d9a855f
Diego Alonso Rubí Salas <diegoalru@gmail.com>
2026-03-21 19:27:40 -0600

    fix(init): Remover dependencia lombok

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -90,7 +90,7 @@ if [ ! -d "backend" ]; then
     -d groupId=com.darssolutionscr \
     -d artifactId=backend \
     -d javaVersion=17 \
-    -d dependencies=web,data-jpa,postgresql,devtools,validation,springdoc-openapi,actuator,lombok \
+    -d dependencies=web,data-jpa,postgresql,devtools,validation,springdoc-openapi,actuator \
     -o backend.zip
   unzip -q backend.zip
   rm backend.zip
